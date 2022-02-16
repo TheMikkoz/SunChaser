23f9ab4e3b76695b967123f23b9fe1cd13d53535
Mikko Jyrkäs <mikko.jyrkas@hotmail.com>
2022-02-16 08:04:06 +0200

    Movement System + Collision Detection

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,38 +8,103 @@ public class Movement : MonoBehaviour
     [SerializeField] private float gravity, speed;
     public bool grounded;
     [SerializeField]private Vector2 movement;
-    
+    [SerializeField]
+    LayerMask groundlayer;
+
     // Start is called before the first frame update
     void Start()
     {
         transform.position = startingPoint;
     }
 
-    // Update is called once per frame
-    void Update()
+    bool Check(RaycastHit2D hit2D)
     {
-        //Inputs
-        inputs();
+        if (hit2D.collider != null)
+        {
+            if (hit2D.collider.tag == "Ground")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return true;
+        }
 
-        //Gravity
-        if (!grounded) movement.y -= gravity * Time.deltaTime; else movement.y = 0;
-
-        //Move
-        transform.Translate(movement * speed * Time.deltaTime);
-        grounded = false;
     }
-    void Jump()
-    {
-    }
 
-    void inputs()
+
+    void Inputs()
     {
+
+        //raycast
+        RaycastHit2D Down1 = Physics2D.Raycast(new Vector2(transform.position.x - transform.localScale.x / 2, transform.position.y), Vector2.down, transform.localScale.y / 2, groundlayer);
+        RaycastHit2D Down2 = Physics2D.Raycast(new Vector2(transform.position.x + transform.localScale.x / 2, transform.position.y), Vector2.down, transform.localScale.y / 2, groundlayer);
+        RaycastHit2D Right1 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - transform.localScale.y / 2 + 0.1f), Vector2.right, transform.localScale.x / 2, groundlayer);
+        RaycastHit2D Right2 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + transform.localScale.y / 2 - 0.1f), Vector2.right, transform.localScale.x / 2, groundlayer);
+        RaycastHit2D Left1 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - transform.localScale.y / 2 + 0.1f), Vector2.left, transform.localScale.x / 2, groundlayer);
+        RaycastHit2D Left2 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + transform.localScale.y / 2 - 0.1f), Vector2.left, transform.localScale.x / 2, groundlayer);
+
+        Debug.DrawRay(new Vector2(transform.position.x, transform.position.y + transform.localScale.y / 2), Vector2.left, Color.red);
+        Debug.DrawRay(new Vector2(transform.position.x, transform.position.y - transform.localScale.y / 2), Vector2.left, Color.red);
+        Debug.DrawRay(new Vector2(transform.position.x, transform.position.y + transform.localScale.y / 2), Vector2.right, Color.blue);
+        Debug.DrawRay(new Vector2(transform.position.x, transform.position.y - transform.localScale.y / 2), Vector2.right, Color.blue);
+
+        print(Down1.collider + " - " + Down2.collider + " - " + Right1.collider + " - " + Right2.collider + " - " + Left1.collider + " - " + Left2.collider);
+
+        //Ground Check
+        if(!Check(Down1) || !Check(Down2)) 
+        {
+            grounded = true;
+            movement.y = 0;
+        }
+        else
+        {
+            movement.y -= gravity * Time.deltaTime;
+        }
+
+        movement.x = 0;
+
+        if (Check(Right1) && Check(Right2))
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                movement.x = speed;
+            }
+        }
+
+        if (Check(Left1) && Check(Left2))
+        {
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                movement.x = -speed;
+            }
+        }
+
         if (Input.GetAxisRaw("Vertical") > 0 && grounded)
         {
             movement.y = speed * 1.5f;
             grounded = false;
         }
-        movement.x = Input.GetAxisRaw("Horizontal") * speed;
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        //Inputs
+        Inputs();
+
+        
+
+        //Old Gravity
+        //if (!grounded) movement.y -= gravity * Time.deltaTime; else movement.y = 0;
+
+        //Move
+        transform.Translate(movement * speed * Time.deltaTime);
+
+    }
 }
